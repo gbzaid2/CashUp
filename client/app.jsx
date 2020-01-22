e10fc021035b45830486441e65128e85667bae60
John Lee <johnjun.lee91@gmail.com>
2020-01-22 17:30:23 -0500

    demo

---
+++ client/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+
+ReactDOM.render(<App/>, document.getElementById('app'))
