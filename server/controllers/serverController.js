2b6310c26d5573cf803290260bb057dffd7f49ec
Gabriel Ng <kingwai715@gmail.com>
2020-01-23 12:25:17 -0500

    update to server controllers

--- server/controllers/serverController.js
+++ server/controllers/serverController.js
@@ -3,12 +3,21 @@ const serverModel = require("../models/serverModel.js")
 
 module.exports = {
   getTransactions: (req, res) => {
-
+    serverModel
+      .("Insert server model")
+      .then(data => res.send(data))
+      .catch(err => console.log(err));
   },
   getCategories: (req, res) => {
-
+    serverModel
+      .("Insert server model")
+      .then(data => res.send(data))
+      .catch(err => console.log(err));
   },
   getBudgets: (req, res) => {
-    
+    serverModel
+      .("Insert server model")
+      .then(data => res.send(data))
+      .catch(err => console.log(err));
   }
 };
