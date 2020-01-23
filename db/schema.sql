b747c37e0e98637e4cc92ef0b6c0515b012686e1
Zaid <gbzaid2@gmail.com>
2020-01-23 12:24:35 -0500

    Added Schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,24 @@
-DROP DATABASE cashup;
-CREATE DATABASE IF NOT EXISTS cashup;
+DROP DATABASE IF EXISTS cashup;
+
+CREATE DATABASE cashup;
+
+USE cashup;
 
 
+CREATE TABLE categories (
+  id int NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL UNIQUE,
+  target int,
+  current int,
+  PRIMARY KEY (ID)
+);
 
 CREATE TABLE transactions (
-    id INT NOT NULL AUTO_INCREMENT,
-    date VARCHAR(20) NOT NULL,
-    description VARCHAR(100) NOT NULL,
-    amount DECIMAL(10,2)
-
-)
+  id int NOT NULL AUTO_INCREMENT,
+  date VARCHAR(50) NOT NULL,
+  amount decimal(10, 2) NOT NULL,
+  description varchar(50) NOT NULL,
+  category_id int,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (category_id) REFERENCES categories(ID)
+);
