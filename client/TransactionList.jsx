a6288f2510b81e8e947467f847b10501dc6866d5
John Lee <johnjun.lee91@gmail.com>
2020-01-23 10:17:06 -0500

    Create React skeleton to handle transaction input and list.

---
+++ client/TransactionList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class TransactionList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="txn">
+                <h3>Your Transactions</h3>     
+
+                <div className="txn-table">
+
+                    <div className="txn-header txn-row">
+                        <div className="txn-data">Date</div>
+                        <div className="txn-data">Description</div>
+                        <div className="txn-data">Amount</div>
+                        <div className="txn-data">Category</div>
+                    </div>
+
+                    {this.props.transactions.map((transaction) => {
+                        return (
+                            <div className="txn-row" key={transaction.id}>
+                                <div className="txn-data">{transaction.date}</div>
+                                <div className="txn-data">{transaction.description}</div>
+                                <div className="txn-data">{transaction.amount}</div>
+                            </div>
+
+                        )
+                    })}
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default TransactionList;
--- client/app.jsx
+++ client/app.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import TransactionInput from './TransactionInput.jsx'
+import TransactionList from './TransactionList.jsx'
+
 class App extends React.Component {
     constructor() {
         super()
+        this.state = {
+            transactions: []
+        }
     }
 
     render() {
         return (
             <div>
+                Hello World
+                <TransactionInput/>
+                <br/>
+
+                <TransactionList transactions={this.state.transactions}/>
 
             </div>
         )
     }
 }
 // this is a coment
-<<<<<<< HEAD
 // another comment
-=======
->>>>>>> demoBranch
 
 
 ReactDOM.render(<App/>, document.getElementById('app'))
